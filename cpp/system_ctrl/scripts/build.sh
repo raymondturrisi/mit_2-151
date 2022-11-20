a20bc58ba314004f1a64542f58115d11336916d5
raymond turrisi <raymond.turrisi@gmail.com>
2022-11-20 14:52:21 -0500

    build script with O3 optimizations for the unit tests thus far

--- cpp/system_ctrl/scripts/build.sh
+++ cpp/system_ctrl/scripts/build.sh
@@ -11,3 +11,7 @@ src/step_test.cpp ../tinkerforge_libc/source/*.o \
 clang++ -O3 -I ../tinkerforge_libc/source/ -I lib/ \
 src/ramp_test.cpp ../tinkerforge_libc/source/*.o \
 -o bin/ramp_test.exe 
+
+clang++ -O3 -I ../tinkerforge_libc/source/ -I lib/ \
+src/frq_test.cpp ../tinkerforge_libc/source/*.o \
+-o bin/frq_test.exe 
